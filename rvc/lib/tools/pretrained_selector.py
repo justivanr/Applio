d1312b2036bdc6e4bcf93ab5fe747e36fc74a710
Ivan Rinaldi <ivanrinal20@gmail.com>
2025-02-20 10:59:22 +0100

    Fix in pretrained models paths

--- rvc/lib/tools/pretrained_selector.py
+++ rvc/lib/tools/pretrained_selector.py
@@ -7,7 +7,7 @@ applio_dir = applio_dir[:applio_dir_len]
 
 
 def pretrained_selector(vocoder, sample_rate):
-    base_path = os.path.join(applio_dir, "rvc", "models", "pretraineds", f"pretrained_{version}")
+    base_path = os.path.join(applio_dir, "rvc", "models", "pretraineds", f"{vocoder.lower()}")
 
     path_g = os.path.join(base_path, f"f0G{str(sample_rate)[:2]}k.pth")
     path_d = os.path.join(base_path, f"f0D{str(sample_rate)[:2]}k.pth")
